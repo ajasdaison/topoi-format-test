b8851155b15a744798b3e87d733ba5259e7692f9
Ajas Daison <ajas.daison@gmail.com>
2025-03-16 00:18:07 +0530

    added_shell_script

--- engine/app/main.cc
+++ engine/app/main.cc
@@ -16,67 +16,64 @@
 #include "topoi/Utils.hh"
 #include "topoi/topoi.h"
 
-struct Options {
-  uint64_t seed = 42;  topoi::index_t levels = 6;
-  topoi::index_t sample_moves = 10;
-  template <class App>
-  void setup_onto(App& app) {
-    // clang-format off
-      app.add_option("--seed", seed, "Random seed");      app.add_option("--levels", levels, "Levels");
-      app.add_option("--sample-moves", sample_moves, "Sample moves");
-    // clang-format on
-  }
+struct Options { uint64_t seed = 42;
+    topoi::index_t levels = 6;
+    topoi::index_t sample_moves = 10;
+    template <class App>
+    void setup_onto(App& app) {
+        // clang-format off
+      app.add_option("--seed", seed, "Random seed");      app.add_option("--levels", levels, "Levels");app.add_option("--sample-moves", sample_moves, "Sample moves");
+        // clang-format on
+    }
 };
 
 static void run(Options options) {
-  using namespace topoi;
-  std::mt19937_64 rng(options.seed);
+    using namespace topoi;  std::mt19937_64 rng(options.seed);
 
-  index_t levels = options.levels;
-  for (size_t i = 0; i < BOARD_CELLS; i++) {
-    Notation id = notation_from_uuid(i);
-    index_t uuid = uuid_from_notation(id);
-    assert(uuid == i);
-  }
+    index_t levels = options.levels;
+    for (size_t i = 0; i < BOARD_CELLS; i++) {
+        Notation id = notation_from_uuid(i);
+        index_t uuid = uuid_from_notation(id);
+        assert(uuid == i);
+    }
 
-  Topoi topoi(levels);
+    Topoi topoi(levels);
 
-  TOPOI_LOG("levels: " << levels << '\n');
-  TOPOI_LOG("cells: " << topoi.adj().size() << '\n');
+    TOPOI_LOG("levels: " << levels << '\n');
+    TOPOI_LOG("cells: " << topoi.adj().size() << '\n');
 
-  Player player = DEFAULT_STARTING_PLAYER;
-  State state = make_initial_state(player);
-  generate_moves(rng, topoi, state, options.sample_moves);
+    Player player = DEFAULT_STARTING_PLAYER;
+    State state = make_initial_state(player);
+    generate_moves(rng, topoi, state, options.sample_moves);
 
-  if (false) {
-    Slide slide{.group = Group({82}), .direction_id = 0, .steps = 1};
-    Leap leap{.source = 116, .target = 100};
-    Step step{.source = 116, .target = 100};
+    if (false) {
+        Slide slide{.group = Group({82}), .direction_id = 0, .steps = 1};
+        Leap leap{.source = 116, .target = 100};
+        Step step{.source = 116, .target = 100};
 
-    std::vector<Move> moves = {
-        Move{.type = Type::Slide, .data = &slide},  //
-        Move{.type = Type::Leap, .data = &leap},    //
-        Move{.type = Type::Step, .data = &step}     //
-    };
+        std::vector<Move> moves = {
+            Move{.type = Type::Slide, .data = &slide},  //
+            Move{.type = Type::Leap, .data = &leap},    //
+            Move{.type = Type::Step, .data = &step}     //
+        };
 
-    replay(topoi, moves);
-  }
+        replay(topoi, moves);
+    }
 }
 
 int main(int argc, char* argv[]) {
-  using namespace topoi;
+    using namespace topoi;
 
-  Options options;
-  CLI::App app{"topoi"};
-  options.setup_onto(app);
+    Options options;
+    CLI::App app{"topoi"};
+    options.setup_onto(app);
 
-  try {
-    app.parse(argc, argv);
-    run(options);
-  } catch (const CLI::ParseError& e) {
-    exit(app.exit(e));
-  }
+    try {
+        app.parse(argc, argv);
+        run(options);
+    } catch (const CLI::ParseError& e) {
+        exit(app.exit(e));
+    }
 
-  return 0;
+    return 0;
 }
-
